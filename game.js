b3a8bc890acaa045b597d1e4973deded180a9657
Alexandre Previ <alexandre.previ@gmail.com>
2019-10-03 20:13:35 +0200

    created class Quiz

--- game.js
+++ game.js
@@ -1,3 +1,55 @@
+class Quiz {
+    constructor(userName, amountQuestion){
+        this.allQuestions = [];
+        this.correctAnswer = 0;
+        this.wrongAnswer = 0;
+        this.currentQuestion;
+        this.questionCounter = 0;
+        this.questionIndex = 0;
+        this.userName = userName;
+        this.amountQuestion = amountQuestion;
+    }
+    displayNewQuestion() {
+        
+        if (this.questionCounter >= this.amountQuestion.value) {
+            
+        // end game
+        
+        if(this.correctAnswer == this.amountQuestion.value){
+            // excellent 10 / 10
+            displayResultsMain.innerText = "Excellent!"
+        } else if ((this.correctAnswer / this.amountQuestion.value) <= 0.9 && (this.correctAnswer / this.amountQuestion.value) >= 0.5) {
+            // good job from 05 / 10 to 09 / 10
+            displayResultsMain.innerText = "Good job!"
+        } else {
+            // could be better from 01 / 10 to 04 / 10
+            displayResultsMain.innerText  = "Better luck next time!"
+        }
+        displayResultsP.innerText = `${this.userName.value}, you got ${this.correctAnswer} correct answers out of ${this.amountQuestion.value} questions!`;
+        return;
+        }
+    
+        // update question counter
+        this.questionCounter++;
+        questionCounterText.innerText = this.questionCounter + "/" + this.amountQuestion.value;
+    
+        // get question
+        this.currentQuestion = quiz1.allQuestions[this.questionIndex];
+        question.innerText = this.currentQuestion.question;
+    
+        // get category of this question
+        category.innerText = this.currentQuestion.category;
+    
+        // get the possible choices for this question
+        for (let i = 0; i < choices.length; i++) {
+            choices[i].innerText = this.currentQuestion.choices[i];
+        }
+        // update question counter
+        this.questionIndex++;
+    };
+    
+}
+
 class Question {
     constructor(category, question, choices, answer) {
         this.category = category;
@@ -5,19 +57,23 @@ class Question {
         this.choices = choices;
         this.answer = answer;
     }
-
 }
 
 
-let allQuestions = [];
+
+let quiz1 = new Quiz(userName, amountQuestion);
+
+
+console.log(quiz1);
 
 
 let json = getJSON('http://www.mocky.io/v2/5d9602fa330000869a2f8e7e');
 
 for (let question of json) {
     let x = new Question(question.category, question.question, question.choices, question.answer);
-    allQuestions.push(x);
+    quiz1.allQuestions.push(x);
 }
+
 
 
 const question = document.getElementById("question");
@@ -30,64 +86,19 @@ const displayResultsMain = document.getElementById("results-main");
 const displayResultsP = document.getElementById("results-p");
 
 
-///// Quiz Variables /////
-let currentQuestion;
-let questionCounter = 0;
-let correctAnswer = 0;
-let wrongAnswer = 0;
 
-function getUserInfo(){
-    const userName = document.getElementById("userName");
-    const amountQuestion = document.getElementById("amountQuestion");
-}
 
 function playQuiz() {
-    
-    getUserInfo();
-    questionCounter = 0;
-    wrongAnswer = 0;
-    correctAnswer = 0;
-    questionIndex = 0;
-
-    displayNewQuestion();
+    getInputs();
+    quiz1.displayNewQuestion();
 };
 
-function displayNewQuestion() {
-    if (questionCounter >= amountQuestion.value) {
-    // end game
-    
-    if(correctAnswer == amountQuestion.value){
-        // excellent 10 / 10
-        displayResultsMain.innerText = "Excellent!"
-    } else if ((correctAnswer / amountQuestion.value) <= 0.9 && (correctAnswer / amountQuestion.value) >= 0.5) {
-        // good job from 05 / 10 to 09 / 10
-        displayResultsMain.innerText = "Good job!"
-    } else {
-        // could be better from 01 / 10 to 04 / 10
-        displayResultsMain.innerText  = "Better luck next time!"
-    }
-    displayResultsP.innerText = `${userName.value}, you got ${correctAnswer} correct answers out of ${amountQuestion.value} questions!`;
-    return;
+function getInputs() {
+    userName = document.getElementById("userName").value;
+    amountQuestion = document.getElementById("amountQuestion").value;
     }
 
-    // update question counter
-    questionCounter++;
-    questionCounterText.innerText = questionCounter + "/" + amountQuestion.value;
 
-    // get question
-    currentQuestion = allQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
-
-    // get category of this question
-    category.innerText = currentQuestion.category;
-
-    // get the possible choices for this question
-    for (let i = 0; i < choices.length; i++) {
-        choices[i].innerText = currentQuestion.choices[i];
-    }
-    // update question counter
-    questionIndex++;
-};
 
 ///// Event Listener /////
 
@@ -98,11 +109,11 @@ for (let i = 0; i < choices.length; i++) {
         // Apply class correct or incorrect according to player answer
         let classToApply = 'incorrect';
 
-        if (choices[i].innerText == currentQuestion.answer) {
+        if (choices[i].innerText == quiz1.currentQuestion.answer) {
             classToApply = 'correct';
-            correctAnswer++;
+            quiz1.correctAnswer++;
         } else {
-            wrongAnswer++;
+            quiz1.wrongAnswer++; 
         }
 
         choices[i].classList.add(classToApply);
@@ -110,7 +121,7 @@ for (let i = 0; i < choices.length; i++) {
         // animation for right/wrong answers with set timeout
         setTimeout(() => {
             choices[i].classList.remove(classToApply);
-            displayNewQuestion();
+            quiz1.displayNewQuestion();
         }, 1000);
 
     });
